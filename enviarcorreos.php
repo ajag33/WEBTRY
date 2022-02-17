02d844c1eac7124b1b9e2014cc6ac4f20b806ebe
JFranciscoGB <j.balcazar.f@gmail.com>
2022-02-17 18:24:17 -0500

    phpmail v1.1

--- enviarcorreos.php
+++ enviarcorreos.php
@@ -20,8 +20,8 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.office365.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'fgutierrez@toshiko.com.pe';                     //SMTP username
-    $mail->Password   = 'Toshikofg$1';                               //SMTP password
+    $mail->Username   = 'hi@toshiko.com.pe';                     //SMTP username
+    $mail->Password   = 'tucu$1';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`  ENCRYPTION_SMTPS
 
