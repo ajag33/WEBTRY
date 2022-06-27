68997f8796cdfae062eedc6d2a55dd2eaca9d067
ajag33 <55101799+ajag33@users.noreply.github.com>
2022-06-27 18:52:01 -0500

    update enviarcorreos2

--- enviarcorreos2.php
+++ enviarcorreos2.php
@@ -44,8 +44,8 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.office365.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'jabad@toshiko.com.pe';                     //SMTP username
-    $mail->Password   = 'Aj@g100612';                               //SMTP password
+    $mail->Username   = 'ajag331@gmail.com';                     //SMTP username
+    $mail->Password   = 'ajag100612';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`  ENCRYPTION_SMTPS
 
