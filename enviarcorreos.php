440491a00792fe3a44ff07a2a084c61fa7082cb1
franciscogb <j.balcazar.f@gmail.com>
2022-06-27 19:31:27 -0500

    Update enviarcorreos.php

--- enviarcorreos.php
+++ enviarcorreos.php
@@ -36,7 +36,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'j.balcazar.f@gmail.com';                     //SMTP username
-    $mail->Password   = 'DELVX952213@.18';                               //SMTP password
+    $mail->Password   = 'ygorrurjgrnutzfp';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`  ENCRYPTION_SMTPS
 
