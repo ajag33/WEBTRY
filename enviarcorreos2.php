c5177385483d1e05962666d5b804105a5e67ac61
ajag33 <55101799+ajag33@users.noreply.github.com>
2022-06-27 19:41:38 -0500

    update enviarcorreo2

--- enviarcorreos2.php
+++ enviarcorreos2.php
@@ -45,7 +45,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'ajag331@gmail.com';                     //SMTP username
-    $mail->Password   = 'ajag100612';                               //SMTP password
+    $mail->Password   = 'wcufngpbuvqqrdfi';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`  ENCRYPTION_SMTPS
 
