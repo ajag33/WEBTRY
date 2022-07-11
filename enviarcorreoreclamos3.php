20ab483785100972410101e94e45ce55a32d6e2e
franciscogb <j.balcazar.f@gmail.com>
2022-07-11 16:29:25 -0500

    NOMBRES

--- enviarcorreoreclamos3.php
+++ enviarcorreoreclamos3.php
@@ -49,8 +49,8 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'ajag331@gmail.com';                     //SMTP username
-    $mail->Password   = 'wcufngpbuvqqrdfi';                               //SMTP password
+    $mail->Username   = 'grupotoshiko19@gmail.com';                    //SMTP username
+    $mail->Password   = 'cmvnghcphgeortip';                              //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`  ENCRYPTION_SMTPS
 
